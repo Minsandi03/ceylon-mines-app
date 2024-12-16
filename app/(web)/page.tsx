0bd0c62fd13b7966dfe7a2a6166242a1a4856d3a
Nadun Nissanka <160146165+Minsandi03@users.noreply.github.com>
2024-12-16 18:49:19 +0530

    adding features

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -3,13 +3,14 @@ import Stats from "./sections/stats";
 import Mission from "./sections/mission";
 import Team from "./sections/team";
 import Footer from "./sections/footer";
-
+import Features from "./sections/features";
 export default function WebPage() {
   return (
     <>
       <HeroSection />
       <Stats />
       <Mission />
+      <Features />
       <Team />
       <Footer />
     </>
